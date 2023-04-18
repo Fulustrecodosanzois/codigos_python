c2d71872103a0f1e6ce23459b4f35d929a97bd18
Luciano Silva <luciano_jet@hotmail.com>
2023-04-17 21:01:57 -0300

    criado aula 2

---
+++ aula02.py
@@ -0,0 +1,20 @@
+nome = input("Fala o nome ai miseravi: ")
+idade = input("Qual sua idade: ")
+
+# Isso é um comentário de uma linha
+
+"""
+    isto é um comentário de várias linhas
+"""
+
+
+
+
+# Primeira forma de concatenar  
+
+#print("Olá ", nome, "sua idade é ", idade)
+print ("Olá {} sua idade é {}".format(nome,idade))
+
+# com F string
+
+print (f"Olá {nome} sua idade é {idade}")
