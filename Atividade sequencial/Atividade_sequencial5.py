e951a81b691e6af1d05dcf039fc42d7494e671b0
Luciano Silva <luciano_jet@hotmail.com>
2023-04-20 19:47:39 -0300

    finalizado Atividade_sequencial

---
+++ Atividade sequencial/Atividade_sequencial5.py
@@ -0,0 +1,9 @@
+""" 5. Um barril de refresco é feito com 8 partes (80%) de água mineral e 2 partes (20%) de suco de maracujá. Faça um algoritmo para calcular quantos litros de água e de suco são necessários para se fazer X litros de refresco. A quantidade de litros de refresco deve ser informada pelo usuário """
+
+litros = int(input("Qual a quantidade de refresco desejada:  "))
+
+agua = litros * 0.8
+suco = litros * 0.2
+
+print(f"O total de água é {agua} e o total de suco é {suco}\n")
+
