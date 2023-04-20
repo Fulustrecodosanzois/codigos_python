fbf81d0a63a96501cb46d473fb4f0a23063b3038
Luciano Silva <luciano_jet@hotmail.com>
2023-04-20 20:48:46 -0300

    finalizado aula 05

---
+++ Aula05-condicional.py
@@ -0,0 +1,12 @@
+nota1= float(input("Informe a nota 1: "))
+nota2= float(input("Informe a nota 2: "))
+
+media = (nota1 + nota2)/2
+
+if media >= 7: 
+    print("Você passou!!!\n")
+elif media == 6: #então se (sempre antes do else)
+    print("Você está de")
+else: #se não
+    print("Você reprovou, seu burro")
+
