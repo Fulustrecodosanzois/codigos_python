face5ee9e26b3c1e12ce2342d60f276dfa27becd
Luciano Silva <luciano_jet@hotmail.com>
2023-04-20 21:07:12 -0300

    finalizado atividade condicional 1

---
+++ Atividade Condicional/Atividade-condicional01.py
@@ -0,0 +1,6 @@
+num = int(input("informe um valor qualquer: "))
+
+if num >= 100 and num <= 200:
+    print(f"O Valor {num} esta entre 100 e 200\n")
+else:
+    print(f"O valor {num} não está entre 100\n")
