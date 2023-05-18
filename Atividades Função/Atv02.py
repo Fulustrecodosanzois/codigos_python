84e02e1ed2f2157038e072f665368060ac184da6
Luciano Silva <luciano_jet@hotmail.com>
2023-05-18 20:22:00 -0300

    realizado atividade 2 função

--- Atividades Função/Atv02.py
+++ Atividades Função/Atv02.py
@@ -1,8 +1,28 @@
-def soma (a, b):
+while True: # enquanto A for diferente de B o loop continua
     a= int(input("Digite um valor: "))
     b= int(input("Digite outro valor: "))
-    if a == b:
-        a= int(input("Digite um valor: "))
-        b= int(input("Digite outro valor: ")) 
-    if a < b:
+    if a != b:
+        break
         
+def Soma_impar (inicial, final):
+    soma = 0
+    for cont in range (inicial, final+1):
+        if cont % 2 == 1:
+            soma+= cont
+    return soma
+
+def media_multiplo3(inicial, final):
+    total= 0
+    contDivisores = 0
+    for cont in range (inicial, final+1):
+        if cont % 3 == 0:
+            total+= cont #isto é para somar todos os divisores
+            contDivisores += 1 # isto é para contar todos os divisores
+            
+    return total / contDivisores
+
+if a < b:
+    print(f"A soma dos valores é: {Soma_impar(a,b)}")
+    
+else:
+    print(media_multiplo3(b,a))
