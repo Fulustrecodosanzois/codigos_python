e25fdab8cd34734c1f9fe8efc99d6bbb89f4dd82
Luciano Silva <luciano_jet@hotmail.com>
2023-05-17 21:10:29 -0300

    respondendo atividade função

---
+++ Atividades Função/Atv02.py
@@ -0,0 +1,8 @@
+def soma (a, b):
+    a= int(input("Digite um valor: "))
+    b= int(input("Digite outro valor: "))
+    if a == b:
+        a= int(input("Digite um valor: "))
+        b= int(input("Digite outro valor: ")) 
+    if a < b:
+        
