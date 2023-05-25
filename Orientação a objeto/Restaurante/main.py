be903f2bc87e7b6c2bbed7da04b29d334faefae0
Fulustrecodosanzois <luciano_jet@hotmail.com>
2023-05-25 11:34:00 -0300

    finalizado atividade restaurante

---
+++ Orientação a objeto/Restaurante/main.py
@@ -0,0 +1,8 @@
+from restaurante import Servico
+
+pedido= Servico()  
+
+
+pedido.novo_pedido()
+pedido.cancelar_pedido()
+pedido.exibir_pedido()
--- Orientação a objeto/Restaurante/restaurante.py
+++ Orientação a objeto/Restaurante/restaurante.py
@@ -1,1 +1,21 @@
-class Servico
+class Servico:
+    def __init__(self, pedido=0):
+        self.__pedido= pedido
+        
+    def novo_pedido(self):
+        for perc in range(1,5):
+            escolha= input("Qual seu pedido: ")
+            self.__pedido+= 1
+        print(f"Seu total do pedido é {self.__pedido}")
+        
+    def cancelar_pedido(self):
+        for perc in range(1,5):
+            escolha= input(f"Deseja cancelar o {perc}° pedido feito?\n sim ou não?  ")
+            if escolha == "sim":
+                self.__pedido-= 1
+                
+            elif escolha == "não":
+                print("O pedido não foi cancelado")
+    
+    def exibir_pedido(self):
+        return print(f"Seu número de pedidos agora é {self.__pedido}")
