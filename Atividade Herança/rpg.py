a3de083f1a132a9bb8c7afb83f1f5f26d18c877c
Luciano Silva <luciano_jet@hotmail.com>
2023-05-25 21:06:42 -0300

    continuando atividade rpg

---
+++ Atividade Herança/rpg.py
@@ -0,0 +1,9 @@
+class Personagem:
+    def __init__(self, nome, vida):
+        self.nome= nome
+        self.vida= vida
+        
+    def atacar(self):
+        print(f"O personagem esta atacando")
+    
+    
