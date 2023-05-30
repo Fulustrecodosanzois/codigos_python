02c08ecc5a1d8449a42107d4703c85ca3b5315e6
Luciano Silva <luciano_jet@hotmail.com>
2023-05-30 20:07:55 -0300

    FINALIZADO INTEGRAÇÃO COM BANCO DE Banco_de_dados

---
+++ Banco_de_dados/funcionario.py
@@ -0,0 +1,43 @@
+import pymysql
+
+class Funcionario:
+    
+    def conexao(self):
+        con= pymysql.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="departamento_db"
+        )
+        
+        return con
+
+    def inserir(self, codigo, nome, salario, cargo):
+        conexao= self.conexao() #está chamando o método que irá logar no banco
+        
+        comando= "insert into funcionario values(%s, %s, %s,%s )"
+        
+        valores= (codigo, nome, salario, cargo)
+        
+        consulta= conexao.cursor()
+        
+        consulta.execute(comando, valores)
+        
+        conexao.commit()
+        
+        print(consulta.rowcount, "linha inserida com sucesso\n")
+        
+        conexao.close()
+        
+    def consultar(self):
+        conexao= self.conexao()
+        
+        comando= "select * from funcionario"
+        consulta= conexao.cursor()
+        consulta.execute(comando)
+        
+        resultado= consulta.fetchall()
+        
+        print(resultado, "\n")
+        
+        conexao.close()
---
+++ Banco_de_dados/main.py
@@ -0,0 +1,7 @@
+from funcionario import Funcionario
+
+f1= Funcionario()
+
+f1.inserir(23, "José", 2200, "traficante")
+
+f1.consultar()
